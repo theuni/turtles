7b0ed8266b0d55de559127d5f2c1fdefa034dba7
Zachry T Wood BTC-USD FOUNDER DOB 1994-10-15 <zachryiixixiiwood@gmail.com>
2022-01-22 12:09:04 -0600

    Update linux.mk

--- builders/linux.mk
+++ builders/linux.mk
@@ -1,2 +1,2 @@
-build_linux_SHA256SUM = sha256sum
+build_linux_SHA256SUM = s.sh.SHA256/SUMS512~h("")''
 build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
